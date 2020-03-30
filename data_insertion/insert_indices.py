a6135630cfd41ac2d3e4c00a98b5e410fae135a6
fercampos19 <fjce19@gmail.com>
2020-03-29 20:55:07 -0600

    creacion del script para insertar indices

---
+++ data_insertion/insert_indices.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+## connect to the db
+host = "localhost"
+db = "conection_test"
+user = "postgres"
+pw = "123"
+
+conn = psycopg2.connect(
+    host = host,
+    database = db,
+    user = user,
+    password = pw)
+
+cur = conn.cursor()
+
+## insert data
+index = input("what is the index you want to register:\n")
+
+cur.execute ('''
+INSERT INTO indices (name) VALUES (%s);
+''', (index,))
+
+## save the data in the database
+conn.commit()
+
+## close the connection
+cur.close()
+conn.close()
+
+print('done')
